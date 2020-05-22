527477b7a2633be3c4f5e3da48be766ff1e7daca
Enzo Daneri <enzodaneri@hotmail.com>
2020-05-21 23:09:16 -0300

    agregué handswash app a proyectos

--- src/storeProgramacion.js
+++ src/storeProgramacion.js
@@ -3,7 +3,11 @@ const programacion = [
   {"title": "Book-List",
    "description": "Aplicación con Local Storage realizada en Html, Bootstrap y JavaScript",
    "url": "https://enzodaneri.github.io/book-list/"
-  },  
+  }, 
+  {"title": "HandsWash-App",
+  "description": "Aplicación para lavado de manos por covid-19",
+  "url": "https://https://enzodaneri.github.io/hands-washing/"
+ },  
                  
   {"title": "Lista de Tareas",
    "description": "Crud realizado en Html, Css y JavaScript",
